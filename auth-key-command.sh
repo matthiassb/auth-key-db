334a2e24cd8e4ce649f48559098d397fd7e4aba6
Matthias Brooks <matthias.brooks@live.com>
2016-08-03 02:25:47 -0400

    add auth-key-command.sh

---
+++ auth-key-command.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+fi
+
+HOST=127.0.0.1
+USER=$1
+
+HOMEDIR=$(eval echo ~$USER)
+if [ -r "$HOMEDIR/.ssh/authorized_keys" ]; then
+  cat $HOMEDIR/.ssh/authorized_keys
+fi
+
+curl -s "http://$HOST/ssh/index.php?username=$USER"
