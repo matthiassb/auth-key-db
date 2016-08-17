1932b8164d325eca6afa1ec58baeb15f00a19702
Matthias Brooks <matthias.brooks@live.com>
2016-08-17 14:41:06 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
   define (LDAP_SERVER, "10.1.0.143");
   define (LDAP_DOMAIN, "example.local");
-	define (LDAP_QUERY_USER, "Administrator");
-	define (LDAP_QUERY_PASSWORD, "<password>");
-	define (LDAP_BASE_DN, "DC=example,DC=local");
+  define (LDAP_QUERY_USER, "Administrator");
+  define (LDAP_QUERY_PASSWORD, "Password");
+  define (LDAP_BASE_DN, "DC=example,DC=local");
 ?>
